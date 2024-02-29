00860c96ce7a5bda0442f8206e90ca279ded5ea5
Jared Hirt <jaredhirt@hotmail.com>
2024-02-29 11:04:48 -0800

    Worked with Tyson to: modify the shopping list to contain singletons of ingredients, as opposed to the recipe.

--- grocery_helper/lib/MyShoppingPage.dart
+++ grocery_helper/lib/MyShoppingPage.dart
@@ -1,22 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:grocery_helper/recipe.dart';
 import 'package:provider/provider.dart';
 
 import 'main.dart';
 
-class MyShoppingPage extends StatelessWidget{
+class MyShoppingPage extends StatefulWidget{
+  const MyShoppingPage({super.key});
+
+  @override
+  State<MyShoppingPage> createState() => _MyShoppingPageState();
+}
+
+class _MyShoppingPageState extends State<MyShoppingPage> {
+  List<String> ingredients = [] ;
+  void addToShoppingList(List<Recipe> list){
+    for(var rec in list){
+      for(var item in rec.ingredients){
+        if(!ingredients.contains(item)){
+          ingredients.add(item);
+        }
+      }
+    }
+    ingredients.sort( (a,b) => a.compareTo(b));
+  }
+
+
   @override
   Widget build(BuildContext context) {
     var myAppState = context.watch<MyAppState>();
+    addToShoppingList(myAppState.savedRecipes);
     return ListView(
       children: [
-        Center(
+        const Center(
             child: Padding(
-              padding: const EdgeInsets.all(8.0),
+              padding: EdgeInsets.all(8.0),
               child: Text('Shopping List'),
             ),
           ),
-        for( var item in myAppState.savedRecipes)
-          ItemOnList(ingredient: item.title),
+        for( var item in ingredients)
+          ItemOnList(ingredient: item),
       ],
     );
   }
